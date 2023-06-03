63d1dbd493b61cce3de64277f63b243cbee5cd94
Zbigniew Żołnierowicz <zzolnierowicz@edu.cdv.pl>
2023-06-03 12:07:34 +0200

    chore: remove comments

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,7 +10,6 @@ public class PlayerController : MonoBehaviour
     public float maxVertical = 4.5f;
     public InputAction move;
     
-    // Start is called before the first frame update
     void Start()
     {
         Debug.Log("[INFO] Instancing player controller");
@@ -18,7 +17,6 @@ public class PlayerController : MonoBehaviour
         move.Enable();
     }
 
-    // Update is called once per frame
     void Update()
     {
         var yAxis = move.ReadValue<float>();
